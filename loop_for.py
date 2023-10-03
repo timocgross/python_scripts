7cc9e0641bfc27bd0c5d62793ef62fc2b050d28f
Timo Groß <timo470314@gmail.com>
2023-10-03 12:08:34 +0200

    Created.

---
+++ loop_for.py
@@ -0,0 +1,15 @@
+primes = [2, 3, 5, 7]
+for prime in primes:
+    print(prime)
+
+# Prints out the numbers 0,1,2,3,4 (all numbers up to 5)
+for x in range(5):
+    print(x)
+
+# Prints out 3,4,5 (all numbers in range 3 to 6)
+for x in range(3, 6):
+    print(x)
+
+# Prints out 3,5,7 (all uneven numbers between 3 and 8)
+for x in range(3, 8, 2):
+    print(x)
