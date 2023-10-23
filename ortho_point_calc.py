a3e09230e0590a0afb90d7684d1690fa6e4ffdcf
Timo Groß <timo470314@gmail.com>
2023-10-23 21:23:08 +0200

    .

---
+++ ortho_point_calc.py
@@ -0,0 +1,11 @@
+points = []
+
+count = int(input('Anzahl der fehlenden Punkte: '))
+
+for i in range(1, count + 1):
+    name = input(f'Name des {i}. Punktes: ')
+    y = float(input('Y-Koordinate: '))
+    x = float(input('X-Koordinate: '))
+    points.append(name), points.append(y), points.append(x)
+
+print(points(float[y]))
